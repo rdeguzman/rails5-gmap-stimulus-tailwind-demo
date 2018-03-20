b6929a5f74378604017cec61100a16f7ca942a6f
rdeguzman <rupertdguzman@gmail.com>
2018-03-20 11:10:39 +1100

    Bind gpamview_controller.js to maps index erb. Initialize gmaps from MapsUtility.

--- app/javascript/controllers/gmapview_controller.js
+++ app/javascript/controllers/gmapview_controller.js
@@ -1,8 +1,14 @@
 import { Controller } from "stimulus"
+import MapsUtility from "../utilities/maps_utility"
 
 export default class extends Controller {
 
   initialize() {
+    let opts = {
+      google: window.google || {},
+    }
+    this.gmaps = new MapsUtility(opts)
+    this.gmaps.initMap()
   }
 
 }
