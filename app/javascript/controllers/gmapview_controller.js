db71f49a29ca89e635a649d51f27ab7b8d703085
rdeguzman <rupertdguzman@gmail.com>
2018-03-20 15:18:18 +1100

    Refactor gmapvie_controller to use this.element.id

--- app/javascript/controllers/gmapview_controller.js
+++ app/javascript/controllers/gmapview_controller.js
@@ -4,11 +4,15 @@ import MapsUtility from "../utilities/maps_utility"
 export default class extends Controller {
 
   initialize() {
+  }
+
+  connect() {
     let opts = {
       google: window.google || {},
     }
+    const el = this.element.id
     this.gmaps = new MapsUtility(opts)
-    this.gmaps.initMap('mapdiv')
+    this.gmaps.initMap(el)
   }
 
 }
