1042315a49bda92c6b80da37e61cb55adf516add
rdeguzman <rupertdguzman@gmail.com>
2018-03-21 14:43:16 +1100

    Add tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-import "../stylesheets/main.scss";
+import "../stylesheets/application.scss";
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
