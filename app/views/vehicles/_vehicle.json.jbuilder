5927709fe36dc171da76285167e739302f54aee8
rdeguzman <rupertdguzman@gmail.com>
2018-03-24 11:59:05 +1100

    Remove vehicle scaffold

--- app/views/vehicles/_vehicle.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! vehicle, :id, :name, :latitude, :longitude, :created_at, :updated_at
-json.url vehicle_url(vehicle, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :vehicles
-
   get 'maps/demo_basic'  => 'maps#demo_basic'
   get 'maps/demo_filtering'  => 'maps#demo_filtering'
 
