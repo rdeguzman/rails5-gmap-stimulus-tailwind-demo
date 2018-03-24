2e1a1cc7f17147ea2add6e8a4759fa9609c012be
rdeguzman <rupertdguzman@gmail.com>
2018-03-24 18:26:35 +1100

    Add factory_bot_rails and faker to Gemfile. Update spec/rails_helper.rb to load all files (i.e factory_bot.rb) from spec/support/**

--- Gemfile
+++ Gemfile
@@ -25,3 +25,8 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
+group :test do
+  gem "factory_bot_rails", "~> 4.0"
+  gem 'faker'
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
